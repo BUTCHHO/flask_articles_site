25da505951cb766d48b15ef30d8ea6678573c23e
BUTCHHO <skeletorkoltmm@gmail.com>
2025-02-14 00:01:30 +0300

    second commit

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, redirect, request, url_for
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, login_required, current_user, logout_user
+from flask_login import login_user, login_required, logout_user
 
-from models import db, Post, Account, login_manager
+from models import db, login_manager, Post, Account
+from secret_staff import secret_key
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'foobar key'
+app.config['SECRET_KEY'] = secret_key
 login_manager.init_app(app)
 
 db.init_app(app)
@@ -15,32 +15,24 @@ with app.app_context():
     db.create_all()
 
 
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def home():
-    articles = Post.query.all()
+    articles = Post.get_all()
     return render_template('home.html',  articles = articles)
 
 @app.route('/create', methods=['POST', 'GET'])
 @login_required
 def create():
     if request.method == 'POST':
-        try:
-            title = request.form['title']
-            content = request.form['content']
-            if content and title:
-                print(title, content)
-                data = Post(title= title, content=content)
-                print('data created')
-                db.session.add(data)
-                db.session.commit()
-                print('commited')
+        match Post.create_article(request.form):
+            case True:
                 return redirect(url_for('home'))
-            else:
-                return "Не введён заголовок или содержание"
-        except Exception as err:
-            print(err)
-            return "ошибка при добавлении статьи"
+            case False:
+                return "не введен заголовок или содержимое"
+            case None:
+                #функция возвращает None если возник Exception
+                return "ошибка при добавлении статьи"
+
     else:
         return render_template('create.html')
 
@@ -49,42 +41,33 @@ def create():
 def about():
     return render_template('about.html',)
 
-
 @app.route('/signup', methods=['POST', 'GET'])
 def sign_up():
     if request.method == 'POST':
-        if len(request.form) == 2:
-            name = request.form['name']
-            password_hashed = generate_password_hash(request.form['password'])
-            try:
-                data = Account(name=name,password=password_hashed)
-                db.session.add(data)
-                db.session.commit()
-            except Exception as err:
-                print(err)
-                return 'Ошибка при создании аккаунта'
-            return redirect(url_for('sign_in'))
+        match Account.create_account(request.form):
+            case True:
+                return redirect(url_for('sign_in'))
+            case False:
+                return redirect(url_for('sign_up'))
+            case None:
+                #функция возвращает None если произошёл Exception
+                return "ошибка при добавлении аккаунта"
     return render_template('sign_up.html')
 
 @app.route('/signin', methods=['POST','GET'])
 def sign_in():
     if request.method == 'POST':
-        if len(request.form) == 2:
-            name = request.form['name']
-            password = request.form['password']
-            acc = Account.query.filter_by(name=name).first()
-
-            if check_password_hash(acc.password, password):
-                login_user(acc)
-                return redirect(url_for('home'))
-            return 'не удалось войти'
-        else:
-            print(request.form)
-            redirect(url_for('home'))
+        if (answers_dict := Account.verify_account(request.form))['verified']:
+            login_user(answers_dict['user'])
+            return redirect(url_for('home'))
+        return 'не удалось войти'
     return render_template('sign_in.html')
+
 @app.route('/logout')
 def sign_out():
     logout_user()
     return redirect(url_for('home'))
+
+
 if __name__ == '__main__':
     app.run(debug=True)
--- models.py
+++ models.py
@@ -1,5 +1,8 @@
+from types import NoneType
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, LoginManager
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -8,11 +11,55 @@ class Post(db.Model):
     title = db.Column(db.String(30), nullable = False)
     content = db.Column(db.Text(300), nullable = False)
 
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
+
+    @classmethod
+    def create_article(cls,form):
+        title = form['title']
+        content = form['title']
+        if title and content:
+            article = cls(title=title, content=content)
+            try:
+                db.session.add(article)
+                db.session.commit()
+            except Exception as err:
+                print(err)
+                return None
+            return True
+        return False
+
 class Account(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(20), nullable = False, unique=True)
     password = db.Column(db.String(256), nullable = False)
 
+    @classmethod
+    def create_account(cls, form):
+        name = form['name']
+        password = form['password']
+        if name and password:
+            password_hashed = generate_password_hash(password)
+            account = cls(name=name, password=password_hashed)
+            try:
+                db.session.add(account)
+                db.session.commit()
+            except Exception as err:
+                print(err)
+                return None
+            return True
+        return False
+
+    @classmethod
+    def verify_account(cls,form):
+        name = form['name']
+        password = form['password']
+        if name and password:
+            acc = cls.query.filter_by(name=name).first()
+            if not (isinstance(acc, NoneType)) and check_password_hash(acc.password, password):
+                return {"user": acc, "verified": True}
+        return {"user": None, "verified": False}
 
 @login_manager.user_loader
 def load_user(user_id):
